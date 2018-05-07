be542d4ebab8da1ba47d250e76b44a285a469f72
ekam230 <ekam230@gmail.com>
2018-05-07 21:36:44 +0400

    Update on last origin and add market order

--- BitMex.php
+++ BitMex.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * BitMex PHP REST API
  *
@@ -6,39 +7,55 @@
  * @version 0.1
  * @link https://github.com/y0un1verse/bitmex-api-php
  */
+
 class BitMex {
+
   //const API_URL = 'https://testnet.bitmex.com';
   const API_URL = 'https://www.bitmex.com';
   const API_PATH = '/api/v1/';
   const SYMBOL = 'XBTUSD';
+
   private $apiKey;
   private $apiSecret;
+
   private $ch;
+
   /*
    * @param string $apiKey    API Key
    * @param string $apiSecret API Secret
    */
+
   public function __construct($apiKey = '', $apiSecret = '') {
+
     $this->apiKey = $apiKey;
     $this->apiSecret = $apiSecret;
+
     $this->curlInit();
+
   }
+
   /*
    * Public
    */
+
   /*
    * Get Ticker
    *
    * @return ticker array
    */
+
   public function getTicker() {
+
     $symbol = self::SYMBOL;
     $data['function'] = "instrument";
     $data['params'] = array(
       "symbol" => $symbol
     );
+
     $return = $this->publicQuery($data);
+
     if(!$return || count($return) != 1 || !isset($return[0]['symbol'])) return false;
+
     $return = array(
       "symbol" => $return[0]['symbol'],
       "last" => $return[0]['lastPrice'],
@@ -47,8 +64,11 @@ class BitMex {
       "high" => $return[0]['highPrice'],
       "low" => $return[0]['lowPrice']
     );
+
     return $return;
+
   }
+
   /*
    * Get Candles
    *
@@ -56,10 +76,13 @@ class BitMex {
    *
    * @param $timeFrame can be 1m 5m 1h
    * @param $count candles count
+   * @param $offset timestamp conversion offset in seconds
    *
    * @return candles array (from past to present)
    */
-  public function getCandles($timeFrame,$count) {
+
+  public function getCandles($timeFrame,$count,$offset = 0) {
+
     $symbol = self::SYMBOL;
     $data['function'] = "trade/bucketed";
     $data['params'] = array(
@@ -69,11 +92,16 @@ class BitMex {
       "partial" => "false",
       "reverse" => "true"
     );
+
     $return = $this->publicQuery($data);
+
     $candles = array();
+
     // Converting
     foreach($return as $item) {
-      $time = strtotime($item['timestamp']); // Unix time stamp
+
+      $time = strtotime($item['timestamp']) + $offset; // Unix time stamp
+
       $candles[$time] = array(
         'timestamp' => date('Y-m-d H:i:s',$time), // Local time human-readable time stamp
         'time' => $time,
@@ -82,11 +110,16 @@ class BitMex {
         'close' => $item['close'],
         'low' => $item['low']
       );
+
     }
+
     // Sorting candles from the past to the present
     ksort($candles);
+
     return $candles;
+
   }
+
   /*
    * Get Order
    *
@@ -94,7 +127,9 @@ class BitMex {
    *
    * @return array or false
    */
+
   public function getOrder($orderID,$count = 100) {
+
     $symbol = self::SYMBOL;
     $data['method'] = "GET";
     $data['function'] = "order";
@@ -103,14 +138,19 @@ class BitMex {
       "count" => $count,
       "reverse" => "true"
     );
+
     $orders = $this->authQuery($data);
+
     foreach($orders as $order) {
       if($order['orderID'] == $orderID) {
         return $order;
       }
     }
+
     return false;
+
   }
+
   /*
    * Get Orders
    *
@@ -118,7 +158,9 @@ class BitMex {
    *
    * @return orders array (from the past to the present)
    */
+
   public function getOrders($count = 100) {
+
     $symbol = self::SYMBOL;
     $data['method'] = "GET";
     $data['function'] = "order";
@@ -127,8 +169,10 @@ class BitMex {
       "count" => $count,
       "reverse" => "true"
     );
+
     return array_reverse($this->authQuery($data));
   }
+
   /*
    * Get Open Orders
    *
@@ -136,7 +180,9 @@ class BitMex {
    *
    * @return open orders array
    */
+
   public function getOpenOrders() {
+
     $symbol = self::SYMBOL;
     $data['method'] = "GET";
     $data['function'] = "order";
@@ -144,13 +190,18 @@ class BitMex {
       "symbol" => $symbol,
       "reverse" => "true"
     );
+
     $orders = $this->authQuery($data);
+
     $openOrders = array();
     foreach($orders as $order) {
       if($order['ordStatus'] == 'New' || $order['ordStatus'] == 'PartiallyFilled') $openOrders[] = $order;
     }
+
     return $openOrders;
+
   }
+
   /*
    * Get Open Positions
    *
@@ -158,22 +209,49 @@ class BitMex {
    *
    * @return open positions array
    */
+
   public function getOpenPositions() {
+
     $symbol = self::SYMBOL;
     $data['method'] = "GET";
     $data['function'] = "position";
     $data['params'] = array(
       "symbol" => $symbol
     );
+
     $positions = $this->authQuery($data);
+
     $openPositions = array();
     foreach($positions as $position) {
       if(isset($position['isOpen']) && $position['isOpen'] == true) {
         $openPositions[] = $position;
       }
     }
+
     return $openPositions;
   }
+
+  /*
+   * Close Position
+   *
+   * Close open position
+   *
+   * @return array
+   */
+
+  public function closePosition($price) {
+
+    $symbol = self::SYMBOL;
+    $data['method'] = "POST";
+    $data['function'] = "order/closePosition";
+    $data['params'] = array(
+      "symbol" => $symbol,
+      "price" => $price
+    );
+
+    return $this->authQuery($data);
+  }
+
   /*
    * Edit Order Price
    *
@@ -184,45 +262,18 @@ class BitMex {
    *
    * @return new order array
    */
+
   public function editOrderPrice($orderID,$price) {
+
     $data['method'] = "PUT";
     $data['function'] = "order";
     $data['params'] = array(
       "orderID" => $orderID,
       "price" => $price
     );
+
     return $this->authQuery($data);
   }
-
-  /*
-   * Edit Order Price NOW
-   *
-   * Edit you open order price
-   *
-   * @param $orderID    Order ID
-   * @param $price      new price
-   *
-   * @return new order array
-   */
-  public function editOrderPriceNow($getOrderID,$position) {
-    $editOrder = array();
-    while ($editOrder != TRUE) {
-      $priceNow = array();
-      while ($priceNow != TRUE){
-        $priceNow=$this->getTicker("XBTUSD");
-        sleep(2);
-      }
-      if ($position =='buy') {$priceNow = $priceNow["bid"];}
-      else {$priceNow = $priceNow["ask"];}
-      echo "Get price OK. Price=".$priceNow."\n";
-      sleep(2);
-      echo ($getOrderID.'\n');
-      $editOrder=$this->editOrderPrice($getOrderID,$priceNow);
-      sleep(2);
-      }
-    echo ("Edit Order OK \n");
-  }
-
 
   /*
    * Create Order
@@ -237,7 +288,9 @@ class BitMex {
    *
    * @return new order array
    */
-  public function createOrder($type,$side,$price,$quantity,$maker = true) {
+
+  public function createOrder($type,$side,$price,$quantity,$maker = false) {
+
     $symbol = self::SYMBOL;
     $data['method'] = "POST";
     $data['function'] = "order";
@@ -248,11 +301,39 @@ class BitMex {
       "orderQty" => $quantity,
       "ordType" => $type
     );
+
     if($maker) {
       $data['params']['execInst'] = "ParticipateDoNotInitiate";
     }
+
     return $this->authQuery($data);
   }
+    /*
+   * Create Order on Market Price
+   *
+   * Create new market order
+   *
+   * @param $type can be "Limit"
+   * @param $side can be "Buy" or "Sell"
+   * @param $price BTC price in USD
+   * @param $quantity should be in USD (number of contracts)
+   * @param $maker forces platform to complete your order as a 'maker' only
+   *
+   * @return new order array
+   */
+  public function createOrderMarket($side,$quantity) {
+    $symbol = self::SYMBOL;
+    $data['method'] = "POST";
+    $data['function'] = "order";
+    $data['params'] = array(
+      "symbol" => $symbol,
+      "side" => $side,
+      "orderQty" => $quantity,
+      "ordType" => 'Market'
+    );    
+    return $this->authQuery($data);
+  }
+
   /*
    * Cancel All Open Orders
    *
@@ -262,7 +343,9 @@ class BitMex {
    *
    * @return all closed orders arrays
    */
+
   public function cancelAllOpenOrders($text = "") {
+
     $symbol = self::SYMBOL;
     $data['method'] = "DELETE";
     $data['function'] = "order/all";
@@ -270,8 +353,10 @@ class BitMex {
       "symbol" => $symbol,
       "text" => $text
     );
+
     return $this->authQuery($data);
   }
+
   /*
    * Get Wallet
    *
@@ -279,14 +364,18 @@ class BitMex {
    *
    * @return array
    */
+
   public function getWallet() {
+
     $data['method'] = "GET";
     $data['function'] = "user/wallet";
     $data['params'] = array(
       "currency" => "XBt"
     );
+
     return $this->authQuery($data);
   }
+
   /*
    * Get Margin
    *
@@ -294,18 +383,66 @@ class BitMex {
    *
    * @return array
    */
+
   public function getMargin() {
+
     $data['method'] = "GET";
     $data['function'] = "user/margin";
     $data['params'] = array(
       "currency" => "XBt"
     );
+
     return $this->authQuery($data);
   }
+
+  /*
+   * Get Order Book
+   *
+   * Get L2 Order Book
+   *
+   * @return array
+   */
+
+  public function getOrderBook($depth = 25) {
+
+    $symbol = self::SYMBOL;
+    $data['method'] = "GET";
+    $data['function'] = "orderBook/L2";
+    $data['params'] = array(
+      "symbol" => $symbol,
+      "depth" => $depth
+    );
+
+    return $this->authQuery($data);
+  }
+
+  /*
+   * Set Leverage
+   *
+   * Set position leverage
+   * $leverage = 0 for cross margin
+   *
+   * @return array
+   */
+
+  public function setLeverage($leverage) {
+
+    $symbol = self::SYMBOL;
+    $data['method'] = "POST";
+    $data['function'] = "position/leverage";
+    $data['params'] = array(
+      "symbol" => $symbol,
+      "leverage" => $leverage
+    );
+
+    return $this->authQuery($data);
+  }
+
   /*
    * Private
    *
    */
+
   /*
    * Auth Query
    *
@@ -315,7 +452,9 @@ class BitMex {
    *
    * @return return array
    */
+
   private function authQuery($data) {
+
     $method = $data['method'];
     $function = $data['function'];
     if($method == "GET" || $method == "POST" || $method == "PUT") {
@@ -337,13 +476,18 @@ class BitMex {
     else {
       $post = $params;
     }
+
     $sign = hash_hmac('sha256', $method.$path.$nonce.$post, $this->apiSecret);
+
     $headers = array();
+
     $headers[] = "api-signature: $sign";
     $headers[] = "api-key: {$this->apiKey}";
     $headers[] = "api-nonce: $nonce";
+
     $headers[] = 'Connection: Keep-Alive';
     $headers[] = 'Keep-Alive: 90';
+
     curl_reset($this->ch);
     curl_setopt($this->ch, CURLOPT_URL, $url);
     if($data['method'] == "POST") {
@@ -365,15 +509,21 @@ class BitMex {
     curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER , false);
     curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
     $return = curl_exec($this->ch);
+
     if(!$return) {
       return $this->curlError();
     }
+
     $return = json_decode($return,true);
+
     if(isset($return['error'])) {
       return $this->platformError($return);
     }
+
     return $return;
+
   }
+
   /*
    * Public Query
    *
@@ -383,65 +533,93 @@ class BitMex {
    *
    * @return return array
    */
+
   private function publicQuery($data) {
+
     $function = $data['function'];
     $params = http_build_query($data['params']);
     $url = self::API_URL . self::API_PATH . $function . "?" . $params;;
+
     $headers = array();
+
     $headers[] = 'Connection: Keep-Alive';
     $headers[] = 'Keep-Alive: 90';
+
     curl_reset($this->ch);
     curl_setopt($this->ch, CURLOPT_URL, $url);
     curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER , false);
     curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers);
     $return = curl_exec($this->ch);
+
     if(!$return) {
       return $this->curlError();
     }
+
     $return = json_decode($return,true);
+
     if(isset($return['error'])) {
       return $this->platformError($return);
     }
+
     return $return;
+
   }
+
   /*
    * Generate Nonce
    *
    * @return string
    */
+
   private function generateNonce() {
+
     $nonce = (string) number_format(round(microtime(true) * 100000), 0, '.', '');
+
     return $nonce;
+
   }
+
   /*
    * Curl Init
    *
    * Init curl header to support keep-alive connection
    */
+
   private function curlInit() {
+
     $this->ch = curl_init();
+
   }
+
   /*
    * Curl Error
    *
    * @return false
    */
+
   private function curlError() {
+
     if ($errno = curl_errno($this->ch)) {
       $errorMessage = curl_strerror($errno);
       echo "cURL error ({$errno}) : {$errorMessage}\n";
       return false;
     }
+
     return false;
   }
+
   /*
    * Platform Error
    *
    * @return false
    */
+
   private function platformError($return) {
+
     echo "BitMex error ({$return['error']['name']}) : {$return['error']['message']}\n";
+
     return false;
   }
+
 }
